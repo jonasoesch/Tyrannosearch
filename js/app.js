78bf543fcd9d306e2307d46ebf22fb63e8fd9025
Jonas Oesch <jonas.oesch@heig-vd.ch>
2014-01-23 16:42:20 +0100

    query function with default values

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 $(document).ready( function() {
 	
 });
+
+function querySolr(query) {
+  if(query.text == null) {query.text = "*:*";}  
+  if(query.roles == null) {query.role = [];}
+  if(query.tags == null) {query.tags = [];}
+  if(query.groups == null) {query.groups [];}
+  if(query.limit == null) {query.limit = 20;}
+  if(query.page == null) {query.page = 1;}
+
+}
