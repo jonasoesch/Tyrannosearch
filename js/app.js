52b42fcd96f8467ea680e31c85627f5cbf174871
Yoann Gern <webyg@icloud.com>
2014-01-30 15:56:08 +0100

    sort results

--- js/app.js
+++ js/app.js
@@ -212,6 +212,7 @@ function querySolr() {
     request['rows'] = state.rows;
     request['fl'] = "*";
     request['wt'] = "json";
+    request['sort'] = sortQuery(state.roles);
     request['facet'] = "true";
     request['facet.field'] = ["role", "tag", "groupname"];
     request['f.tag.facet.limit'] = "5";
@@ -239,6 +240,40 @@ function querySolr() {
 			}
 		}
 	});
+}
+
+
+function sortQuery(roles) {
+    var query = "score desc";
+    
+    var media = ['video', 'audio', 'text', 'image'];
+    
+    var aMedia = false;
+    var aCity = false;
+    var aPerson = false;
+    
+    $(roles).each( function(index, role) {
+        if($.inArray(role, media) >= 0) {
+            aMedia = true;
+        }
+        
+        if(role == 'city') {aCity = true;}
+        if(role == 'person') {aPerson = true;}
+    });
+    
+    if(aMedia && !aCity && !aPerson) {
+        query += ", title asc";
+    }
+    
+    if(aCity && !aMedia && !aPerson) {
+        query += ", city.name asc";
+    }
+    
+    if(aPerson && !aCity && !aMedia) {
+        query += ", lastname asc, firstname asc";
+    }
+    
+    return query;
 }
 
 
